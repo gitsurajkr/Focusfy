8452feedc457b730125359bb5b5a0da475f51e47
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2025-08-25 03:01:35 +0530

    fixed cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // Frontend URL
+  origin: ['http://localhost:3000', 'https://afocusfy.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
